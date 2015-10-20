b5327ceadcab035ce9fe0743529a092a8d7c2e0d
Guillaume Massé <masgui@gmail.com>
2015-10-20 11:55:53 -0400

    decoding DNS hostname

--- src/kata/1_scodec_dns.scala
+++ src/kata/1_scodec_dns.scala
@@ -6,26 +6,50 @@ import scodec.bits._
 
 @instrument class Dns {  
   def bit = bool(1)
+  case class Request(transactionID: Int, name: List[String])
+  def dnsString = new Codec[List[String]] {
+    def sizeBound = SizeBound.unknown
+    override def encode(as: List[String]) = as.foldLeft(Attempt.successful(BitVector.empty)) { (acc, v) =>
+      for {
+        cur <- acc
+        a   <- uint8.encode(v.length)
+        b   <- fixedSizeBytes(v.length.toLong, utf8).encode(v)
+      } yield cur ++ a ++ b
+    }.map(_ ++ BitVector.lowByte)                                                                           
+    override def decode(bits0: BitVector) = {
+      def go(acc: List[String], bits: BitVector): Attempt[DecodeResult[List[String]]] = {
+        uint8.decode(bits) match {
+          case Attempt.Successful(DecodeResult(v, rem)) =>
+            if(v == 0) Attempt.Successful(DecodeResult(acc.reverse, rem))
+            else fixedSizeBytes(v.toLong, utf8).decode(rem) match {
+              case Attempt.Successful(DecodeResult(vs, rem2)) => go(vs :: acc, rem2)
+              case f: Attempt.Failure => f
+            }
+          case f: Attempt.Failure => f
+        }
+      }
+      go(Nil, bits0)
+    }
+  }
   val requestCodec = (
     ("Transaction ID"         | uint16)               ::
     ("Response"               | constant(bin"0"))     :: 
-    ignore(4)                                         :: // ("Opcode" | bits(4)) ::
-    ignore(1)                                         ::
-    ("Truncated"              | bit)                  ::
-    ("Recursion"              | bit)                  ::
-    ignore(3)                                         ::
-    ("Non-authenticated data" | bit)                  ::
-    ignore(4)                                         ::
-    ("Questions"              | ignore(16))           :: // >>:~ ( questions =>
+    ("Opcode"                 | ignore(4))            ::
+    ("Reserved"               | ignore(1))            ::
+    ("Truncated"              | ignore(1))            ::
+    ("Recursion"              | ignore(1))            ::
+    ("Reserved"               | ignore(3))            ::
+    ("Non-authenticated data" | ignore(1))            ::
+    ("Reserved"               | ignore(4))            ::
+    ("Questions"              | ignore(16))           ::
     ("Answer RRs"             | ignore(16))           ::
     ("Authority RRs"          | ignore(16))           ::
     ("Additional RRs"         | ignore(16))           ::
-    ("Name"                   | cstring)              ::
+    ("Name"                   | dnsString)            ::
     ("Type"                   | constant(hex"00 01")) ::
     ("Class"                  | constant(hex"00 01"))
   ).dropUnits.as[Request]
 
-  
   val message = hex"""edf8
                       0100
                       0010
@@ -36,20 +60,5 @@ import scodec.bits._
                       0001
                       0001""".bits
   
-  val name = 
-    requestCodec.decode(message) match {
-      case Attempt.Successful(DecodeResult(Request(_, _, _, _, name), _)) => name
-      case _ => ""
-    }
-
-  name == "askubuntu.com" // 09 ? 03 ?
-  
-  case class Request(
-    transactionID: Int,
-    // opcode: Byte, ignored
-    truncated: Boolean,
-    recursive: Boolean,
-    authorative: Boolean,
-    name: String
-  )
+ requestCodec.decode(message)
 }
