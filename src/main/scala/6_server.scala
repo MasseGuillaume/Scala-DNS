d7a435d224a850e7d9d0211f3575ad1a0e289ea5
Guillaume Massé <masgui@gmail.com>
2015-10-26 08:31:43 -0400

    client server round trip

--- src/main/scala/6_server.scala
+++ src/main/scala/6_server.scala
@@ -6,29 +6,43 @@ import scalaz.concurrent.Task
 import java.net.InetSocketAddress
 import java.util.concurrent.CountDownLatch
 
-object DnsServer extends DnsRequest with DnsResponse {
+trait ClientServer extends DnsRequest with DnsResponse {
 
   val port = 7890
-  dnsString.encode(List("www", "example", "org")).map{ msg =>
-    val addr = new InetSocketAddress("127.0.0.1", port)
-    val latch = new CountDownLatch(1)
-    val maxSize = 1024
-
-    val client = udp.listen(port + 1) {
-      udp.eval_(Task.delay { latch.await }) ++
-      udp.sends_(to = addr, Process.emit(msg.bytes)) ++
-      udp.receive(maxSize)
-    }
-    val server = udp.listen(port) {
-      udp.eval_(Task.delay { latch.countDown }) ++
-      udp.receive(maxSize).map{packet => 
-        dnsString.decode(packet.bytes.bits).fold(
-          _ => Task.now(()),
-          request => request.value
-        )
-        packet.origin
-      }
-    }
-    server.merge(client).runLog.run
+  val addr = new InetSocketAddress("127.0.0.1", port)
+  val latch = new CountDownLatch(1)
+  val maxSize = 1024
+  
+  def asProcess[T](attempt: Attempt[T]): Process0[T] = 
+    Process.emit(
+      attempt.fold(
+        f => throw new Exception(f.messageWithContext),
+        v => v       
+      )
+    )
+  
+  def dnsLogic(request: Request): Response = {
+    val catz = IPV4(104, 131, 51, 57)
+    Response(request.transactionID , request.name, catz)
   }
+  
+  def server(p: Int) = udp.listen(p) {
+    udp.eval_(Task.delay { latch.countDown }) ++
+    (for {
+      packet <- udp.receive(maxSize)
+      request <- asProcess(requestCodec.decode(packet.bytes.bits)).map(_.value)
+      response <- asProcess(responseCodec.encode(dnsLogic(request)))
+      _ <- udp.send(to = packet.origin, response.bytes) 
+    } yield Nil)
+  }
+  def client(p: Int) = udp.listen(p + 1) {
+    udp.eval_(Task.delay { latch.await }) ++
+    (for {
+      request <- asProcess(requestCodec.encode(Request(30144, List("www", "netbsd", "org"))))
+      _ <- udp.send(to = addr, request.bytes)
+      packet <- udp.receive(maxSize)
+      response <- asProcess(responseCodec.decode(packet.bytes.bits))
+    } yield List(response))
+  }
+  // server(port).merge(client(port)).runLog.run
 }
